2e13c14d6dc53016d3b104145afcbb0382476495
Jayesh Singh <jayeshsingh001@gmail.com>
2026-05-01 22:51:41 +0530

    try pydantic BaseModel for post api payload

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 
+# from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+
 app = FastAPI()
 
 
@@ -11,3 +15,15 @@ async def root():
 @app.get("/posts")
 async def get_posts():
     return {"data": "here will be your posts!"}
+
+
+class Post(BaseModel):
+    title: str
+    # content: Optional[str] = None # this or
+    content: str | None = None
+
+
+@app.post("/createposts")
+async def create_posts(payload: Post):
+    print(payload)
+    return {"new_post": payload.title, "content": payload.content}
